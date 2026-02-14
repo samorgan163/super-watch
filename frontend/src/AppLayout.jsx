ed1ef1fff58abc9248d5c42dea25b299729a07db
Sam M <samorgan1603@gmail.com>
2026-02-14 02:23:24 +0000

    added different navbar based on screen width

--- frontend/src/AppLayout.jsx
+++ frontend/src/AppLayout.jsx
@@ -3,13 +3,27 @@ import BottomNavbar from './components/BottomNavbar/BottomNavbar';
 import Navbar from './components/Navbar/Navbar';
 
 function AppLayout({ title, children }) {
+
+	// TODO: change this to detect where touch devices and decide nav bar from there
+	const isMobileLayout = window.matchMedia('(max-width: 768px)').matches;
+	console.log(isMobileLayout);
+
     return (
-      	<>
-			<Navbar />
-			<main>{children}</main>
-			
-      	</>
-    );
+		<>
+			{isMobileLayout ? (
+				<>
+					<TopNavbar />
+					<main>{children}</main>
+					<BottomNavbar />
+				</>
+			) : (
+				<>
+					<Navbar />
+					<main>{children}</main>
+				</>
+			)}
+		</>
+  	);
 }
 
 export default AppLayout;
