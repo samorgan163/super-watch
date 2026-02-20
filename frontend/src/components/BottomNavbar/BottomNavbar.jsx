d120ceffd693acb81589ec8e946e8803f4af51d3
Sam M <samorgan1603@gmail.com>
2026-02-20 03:04:15 +0000

    added icons to mobile navbar

--- frontend/src/components/BottomNavbar/BottomNavbar.jsx
+++ frontend/src/components/BottomNavbar/BottomNavbar.jsx
@@ -1,29 +1,65 @@
 import styles from './BottomNavbar.module.css';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { IoHomeSharp } from "react-icons/io5";
+import { IoHomeOutline } from "react-icons/io5";
+
+import { IoSearchSharp } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+
+import { IoDocumentText } from "react-icons/io5";
+import { IoDocumentTextOutline } from "react-icons/io5";
+
+import { IoPersonSharp } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
 
 import watchlistIcon from '../../assets/icons/watchlist.png';
 
 const navItems = [
-	{ label: 'Dashboard', href: '/', icon: <IoHomeSharp /> },
-	{ label: 'Add', href: '/search', icon: <IoHomeSharp /> },
-	{ label: 'Watchlist', href: '/watchlist', icon: <IoHomeSharp /> },
-	{ label: 'Profile', href: '/profile', icon: <IoHomeSharp /> },
+	{ label: 'Dashboard', href: '/', iconSharp: <IoHomeSharp />, iconOutline: <IoHomeOutline /> },
+	{ label: 'Add', href: '/search', iconSharp: <IoHomeSharp />, iconOutline: <IoHomeOutline /> },
+	{ label: 'Watchlist', href: '/watchlist', iconSharp: <IoHomeSharp />, iconOutline: <IoHomeOutline /> },
+	{ label: 'Profile', href: '/profile', iconSharp: <IoHomeSharp />, iconOutline: <IoHomeOutline /> },
 ];
 
 function BottomNavbar() {
     return (
 		<nav className={styles.navBottomWrapper}>
 			<div className={styles.navBottom}>
-				{navItems.map((item) => (
-					<Link key={item.label} to={item.href} aria-label={item.label}>
-						
-							{item.icon}
-						
-						<p>{item.label}</p>
-					</Link>
-				))}
+				
+					<NavLink key='Dashboard' to='/' end aria-label='Dashboard'>
+						{({ isActive }) => (
+							<>
+								{isActive ? <IoHomeSharp /> : <IoHomeOutline />}
+								<p>Dashboard</p>
+							</>
+						)}
+					</NavLink>
+					<NavLink key='Search' to='/search' aria-label='Search'>
+						{({ isActive }) => (
+							<>
+								{isActive ? <IoSearchSharp /> : <IoSearchOutline />}
+								<p>Search</p>
+							</>
+						)}
+					</NavLink>
+					<NavLink key='Watchlist' to='/watchlist' aria-label='Watchlist'>
+						{({ isActive }) => (
+							<>
+								{isActive ? <IoDocumentText /> : <IoDocumentTextOutline />}
+								<p>Watchlist</p>
+							</>
+						)}
+					</NavLink>
+					<NavLink key='Profile' to='/profile' aria-label='Profile'>
+						{({ isActive }) => (
+							<>
+								{isActive ? <IoPersonSharp /> : <IoPersonOutline />}
+								<p>Profile</p>
+							</>
+						)}
+					</NavLink>
+				
 			</div>
 		</nav>
     );
