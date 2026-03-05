32e36d5702498aff3c383ac44169f9fd2ee4876b
Sam M <samorgan1603@gmail.com>
2026-03-05 12:55:44 +0000

    Watchlist service and controller restructure.

--- backend/controllers/watchlist-controller.js
+++ backend/controllers/watchlist-controller.js
@@ -1,30 +1,4 @@
-const Film = require('../models/film.js');
-const User = require('../models/user.js');
-const tmdbService = require('../services/tmdb/tmdbService.js');
 const watchlistService = require('../services/watchlistService.js');
-
-const { NotFoundError, ConflictError } = require('../errors/customErrors.js');
-
-// once database grows, this can be negated by calling 'getFilmById' directly.
-// only storing a subset of data locally to reduce db size, for now.
-async function createFilmObject(tmdbid) {
-    try {
-        const tmdbRes = await tmdbService.getFilmById(tmdbid);
-
-        const newFilm = {
-            tmdbid: tmdbRes.tmdbid,
-            title: tmdbRes.title,
-            release_date: tmdbRes.release_date,
-            poster: tmdbRes.poster,
-            streaming: tmdbRes.streaming,
-        };
-
-        return newFilm;
-    }
-    catch (error) {
-        throw new Error('tmdb fetch error: ' + error.message);
-    }
-}
 
 /**
  * Adds a film to the user's watchlist.
@@ -35,30 +9,11 @@ async function createFilmObject(tmdbid) {
  * @returns 
  */
 exports.addFilmToWatchlist = async (req, res, next) => {
-    const userId = req.user.id;
-    const tmdbid = req.params.tmdbid;
+    const userID = req.user.id;
+    const tmdbID = req.params.tmdbid;
 
     try {
-        // find or create film for local collection
-        // this uses upsert to avoid duplicates
-        // the checking for film and insertion if not found is done in one db call, making it atomic
-        const film = await Film.findOneAndUpdate(
-            { tmdbid },
-            { $setOnInsert: await createFilmObject(tmdbid) },
-            { new: true, upsert: true }
-        );
-
-        // add films to users watchlist if not already present
-        const result = await User.updateOne(
-            { _id: userId, 'watchlist.film': { $ne: film._id }},
-            { $push: { watchlist: { film: film._id } } }
-        );
-
-        // if no documents were modified, film was already in watchlist
-        if (result.modifiedCount === 0) {
-            throw new ConflictError('Film already in watchlist');
-        }
-
+        await watchlistService.addFilmToWatchlist(userID, tmdbID);
         return res.status(200).json({ message: 'Film added to watchlist' });
     }
     catch (err) {
@@ -75,22 +30,11 @@ exports.addFilmToWatchlist = async (req, res, next) => {
  * @returns 
  */
 exports.removeFilmFromWatchlist = async (req, res, next) => {
-    const userId = req.user.id;
-    const tmdbid = req.params.tmdbid;
-
+    const userID = req.user.id;
+    const tmdbID = req.params.tmdbid;
+    
     try {
-        // find the film from local database, to get the internal _id
-        const film = await Film.findOne({ tmdbid: tmdbid });
-        if (!film) {
-            throw new NotFoundError('Film not found in local database');
-        }
-
-        // remove from user's watchlist
-        await User.updateOne(
-            { _id: userId },
-            { $pull: { watchlist: { film: film._id } } }
-        );
-
+        await watchlistService.removeFilmFromWatchlist(userID, tmdbID);
         return res.status(200).json({ message: 'Film removed from watchlist' });
     }
     catch (error) {
@@ -101,27 +45,16 @@ exports.removeFilmFromWatchlist = async (req, res, next) => {
 // return boolean whether a film is in the user's watchlist
 // frontend can indicate to the user
 // TODO: I dont think this is a good implementation
-exports.filmInWatchlist = async (req, res) => {
-    const userId = req.user.id;
-    const tmdbid = parseInt(req.params.tmdbid, 10);
+exports.filmInWatchlist = async (req, res, next) => {
+    const userID = req.user.id;
+    const tmdbID = parseInt(req.params.tmdbid, 10);
     
     try {
-        
-        const film = await Film.findOne({ tmdbid: tmdbid }).select('_id');
-        
-        if (!film) {
-            return res.status(200).json({ in_watchlist: false });
-        }
-
-        const exists = await User.exists({
-            _id: userId,
-            'watchlist.film': film._id
-        });
-
-        return res.status(200).json({ in_watchlist: Boolean(exists) });
+        const exists = await watchlistService.checkIfFilmInWatchlist(userID, tmdbID);
+        return res.status(200).json({ in_watchlist: exists });
     }
     catch (error) {
-        return res.status(500).json({ message: 'Error checking watchlist: ' + error.message });
+        next(error);
     }
 }
 
@@ -138,7 +71,10 @@ exports.getWatchlist = async (req, res, next) => {
         const filmsStreaming = await watchlistService.getFilmsStreamingInWatchlist(userID);
         const filmsUnavailable = await watchlistService.getFilmsUnavailableInWatchlist(userID);
 
-        return res.status(200).json({ streaming: filmsStreaming, unavailable: filmsUnavailable });
+        return res.status(200).json({ 
+            streaming: filmsStreaming, 
+            unavailable: filmsUnavailable 
+        });
     }
     catch (error) {
         next(error);
--- backend/services/watchlistService.js
+++ backend/services/watchlistService.js
@@ -1,6 +1,87 @@
 const User = require('../models/user.js');
+const Film = require('../models/film.js');
+const tmdbService = require('../services/tmdb/tmdbService.js');
 
-const { NotFoundError } = require('../errors/customErrors.js');
+const { NotFoundError, ConflictError } = require('../errors/customErrors.js');
+
+// once database grows, this can be negated by calling 'getFilmById' directly.
+// only storing a subset of data locally to reduce db size, for now.
+async function createFilmObject(tmdbID) {
+    const tmdbRes = await tmdbService.getFilmById(tmdbID);
+
+    const newFilm = {
+        tmdbid: tmdbRes.tmdbid,
+        title: tmdbRes.title,
+        release_date: tmdbRes.release_date,
+        poster: tmdbRes.poster,
+        streaming: tmdbRes.streaming,
+    };
+
+    return newFilm;
+}
+
+exports.addFilmToWatchlist = async (userID, tmdbID) => {
+    // find or create film for local collection
+    // this uses upsert to avoid duplicates
+    // the checking for film and insertion if not found is done in one db call, making it atomic
+    const film = await Film.findOneAndUpdate(
+        { tmdbid: tmdbID },
+        { $setOnInsert: await createFilmObject(tmdbID) },
+        { new: true, upsert: true }
+    );
+
+    // add films to users watchlist if not already present
+    const result = await User.updateOne(
+        { _id: userID, 'watchlist.film': { $ne: film._id }},
+        { $push: { watchlist: { film: film._id } } }
+    );
+
+    if (result.matchedCount === 0) {
+        throw new NotFoundError('User not found');
+    }
+
+    // if no documents were modified, film was already in watchlist
+    if (result.modifiedCount === 0) {
+        throw new ConflictError('Film already in watchlist');
+    }
+}
+
+exports.removeFilmFromWatchlist = async (userID, tmdbID) => {
+    // find the film from local database, to get the internal _id
+    const film = await Film.findOne({ tmdbid: tmdbID }).select('_id');
+    if (!film) {
+        throw new NotFoundError('Film not found in local database');
+    }
+
+    // remove from user's watchlist
+    const result = await User.updateOne(
+        { _id: userID },
+        { $pull: { watchlist: { film: film._id } } }
+    );
+
+    if (result.matchedCount === 0) {
+        throw new NotFoundError('User not found');
+    }
+
+    if (result.modifiedCount === 0) {
+        throw new NotFoundError('Film not in watchlist');
+    }
+}
+
+exports.checkIfFilmInWatchlist = async (userID, tmdbID) => {
+    const film = await Film.findOne({ tmdbid: tmdbID }).select('_id');
+        
+    if (!film) {
+        return false;
+    }
+
+    const exists = await User.exists({
+        _id: userID,
+        'watchlist.film': film._id,
+    });
+
+    return Boolean(exists);
+}
 
 exports.getFilmsStreamingInWatchlist = async (userID) => {
     const user = await User.findById(userID)
@@ -15,7 +96,9 @@ exports.getFilmsStreamingInWatchlist = async (userID) => {
         throw new NotFoundError('User not found');
     }
 
-    return user.watchlist.filter(item => item.film).map(item => item.film); //filter out null films (ie not streaming) 
+    return user.watchlist
+        .filter(item => item.film)
+        .map(item => item.film); //filter out null films (ie not streaming) 
 }
 
 exports.getFilmsUnavailableInWatchlist = async (userID) => {
@@ -31,5 +114,7 @@ exports.getFilmsUnavailableInWatchlist = async (userID) => {
         throw new NotFoundError('User not found');
     }
 
-    return user.watchlist.filter(item => item.film).map(item => item.film); //filter out null films (ie streaming)
+    return user.watchlist
+        .filter(item => item.film)
+        .map(item => item.film); //filter out null films (ie streaming)
 }
